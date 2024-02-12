1082faf2ad39e5a1a9b1a187db0cb70881de7e89
smithwithatypo <smithwithatypo@gmail.com>
2024-02-11 18:29:22 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+// Routing
+import { AppRoutingModule } from './app-routing.module';
+
+// Forms
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+// Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input'; 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+
+// Http
+import { HttpClientModule } from '@angular/common/http';
+
+// Components
+
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +33,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatSliderModule,
+    MatCardModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
