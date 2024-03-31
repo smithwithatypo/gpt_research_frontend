dfaaf314551174c67b7cc97042ad7157a5b91807
smithwithatypo <smithwithatypo@gmail.com>
2024-03-31 13:55:07 -0500

    remove save text button, add some responsive CSS

--- src/app/view-evaluation/text-input/text-input.component.ts
+++ src/app/view-evaluation/text-input/text-input.component.ts
@@ -10,14 +10,21 @@ export class TextInputComponent {
   @Output() textSubmit = new EventEmitter<string>();
 
   user_code: string = '';
+  saved = false;
 
   constructor() { }
   
   saveText() {
     this.textSubmit.emit(this.user_code);
+    this.saved = true;
+  }
+
+  clearAutosave() {
+    this.saved = false;
   }
 
   clearText() {
     this.user_code = '';
+    this.saved = false;
   }
 }
